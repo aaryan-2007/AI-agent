6154580a7791d450a552673b29c5bbf526bc7527
Kishan Kumar <121043195+kishan-electrodev@users.noreply.github.com>
2025-12-28 14:58:15 +0000

    correct the folder structure

--- src/main.js
+++ src/main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+import { Clerk } from '@clerk/clerk-js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const clerk = new Clerk(clerkPubKey)
+await clerk.load()
 
-setupCounter(document.querySelector('#counter'))
+
+if (clerk.isSignedIn) {
+  document.querySelector('.login-btn').innerHTML = `
+    <div id="user-button"></div>
+  `
+
+  const userButtonDiv = document.getElementById('user-button')
+
+  clerk.mountUserButton(userButtonDiv)
+} else {
+  document.getElementById('app').innerHTML = `
+    <div id="sign-in"></div>
+  `
+
+  const signInDiv = document.getElementById('sign-in')
+
+  clerk.mountSignIn(signInDiv,{
+  afterSignInUrl: '/index.html',
+  afterSignUpUrl: '/index.html',
+})
+}
